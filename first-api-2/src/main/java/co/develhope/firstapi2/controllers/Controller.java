a536fb7d8a95d893c521dd6ee51a837c2e879bf6
Francesco Amarena <ungisk92@gmail.com>
2023-03-10 14:05:51 +0100

    Initial commit

---
+++ first-api-2/src/main/java/co/develhope/firstapi2/controllers/Controller.java
@@ -0,0 +1,18 @@
+package co.develhope.firstapi2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "/factorial/{n}")
+    public long factorial(@PathVariable int n){
+        long fact=1;
+        for (int i=2;i<=n;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+
+}
